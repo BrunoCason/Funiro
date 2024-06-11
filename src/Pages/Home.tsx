11e617acb2150defbc7154fd0dda9e9bdb14b243
Bruno Cason <brunocasons@gmail.com>
2024-06-11 15:22:23 -0300

    feat: add section 5 home and changing images to s3

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,13 @@
 import Sec1Discover from "../Components/Sec1Discover";
 import Sec2Browse from "../Components/Sec2Browse";
+import Sec5Funiro from "../Components/Sec5Funiro";
 
 const Home = () => {
   return (
     <main>
       <Sec1Discover />
       <Sec2Browse />
+      <Sec5Funiro />
     </main>
   );
 };
